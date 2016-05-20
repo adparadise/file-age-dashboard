00c467683ca57149db6fa94653667524b544f723
Andrew Paradise <adparadise@gmail.com>
2016-05-20 12:53:06 -0400

    remove external requirements

--- fileage/command/watch.py
+++ fileage/command/watch.py
@@ -2,7 +2,6 @@ import argparse
 import json
 import os
 import re
-import requests
 import sys
 import time
 
--- runtime/__init__.py
+++ runtime/__init__.py
@@ -1,6 +1,4 @@
 
 
 from runtime import Runtime
-from database_service_factory import DatabaseServiceFactory
-from postgres_database import PostgresDatabase
 from log import Log
